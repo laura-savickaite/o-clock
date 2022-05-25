5843f1233e28c7d4a78e72b8ad61c9264bb7dee7
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-05-25 10:46:09 +0200

    changement dans le css de l'index

--- index.php
+++ index.php
@@ -4,11 +4,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <script src="script.js" charset="utf-8"></script>
     <title>O'Clock</title>
 </head>
 <body>
 
+<nav>
+    <a href="index.php"><p>Accueil</p></a>
+    <a href="horloge.php"><p>Horloge</p></a>
+    <a href="minuteur.php"><p>Minuteur</p></a>
+    <a href="chrono.php"><p>Chronomètre</p></a>
+    <a href="reveil.php"><p>Réveil</p></a>    
+</nav>
 
-</body>
-</html>
+<main id = "main-index">
+
+    <article id="main-section">
+        <section id = "top">
+            <div id = "text-main-section">
+                <h1>Need time?</h1>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error temporibus maiores rem cum debitis quaerat sequi, nam maxime tempora perspiciatis. Illo, ipsam laboriosam. Unde possimus sunt, quam qui aliquam sint.
+                </p>            
+            </div>
+
+            <img src="images/study.jpeg" height="400px">
+        </section>  
+        <section id = "bottom">
+            <img src="images/draw.jpeg" width="150px">
+            <ul>
+                <li><a href="horloge.php">horloge</a></li>
+                <li><a href="minuteur.php">minuteur</a></li>
+                <li><a href="chrono.php">chronomètre</a></li>
+                <li><a href="reveil.php">réveil</a></li>
+            </ul>
+        </section>  
+    </article> 
+    
+
+
+    <h1 id="h1">TIMELAPS</h1>
+</main>
+
+<div id="border"></div>
+
+<footer>
+    <a href="https://github.com/laura-savickaite/o-clock"><img src="image
