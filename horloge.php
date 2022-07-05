3fd8c56cefb33c35e3cac5bd5a7fd33dfe6d0704
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-07-05 15:36:25 +0200

    style horloge fait

--- horloge.php
+++ horloge.php
@@ -5,12 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="scripts/horloge.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="style.css">
     <title>Horloge</title>
 </head>
 <body>
 
+    <nav>
+        <a href="index.php"><p>Accueil</p></a>
+        <a href="horloge.php"><p>Horloge</p></a>
+        <a href="minuteur.php"><p>Minuteur</p></a>
+        <a href="chrono.php"><p>Chronomètre</p></a>
+        <a href="reveil.php"><p>Réveil</p></a>    
+    </nav>
+
     <div id= "horloge">
     </div>
 
+    <footer>
+        <a href="https://github.com/laura-savickaite/o-clock"><img src="images/github.png" width="50px"></a>
+    </footer>
 </body>
 </html>
